71b2c066333b901cee35d99218305cf8cede3ac0
Mahum Haider <mahumhaider@Mahums-MacBook-Air.local>
2021-09-16 17:57:27 -0400

    first commit

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -27,7 +27,8 @@ def create_app(test_config=None):
   CORS(app)
 
 
-  #@TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+  #@TODO: Set up CORS. 
+  CORS(app, resources={"/": {"origins": "*"}})
 
   #CORS Headers
   @app.after_request
@@ -41,10 +42,29 @@ def create_app(test_config=None):
     return response
     
 
+
+  #Create an endpoint to handle GET requests for all available categories.
+  @app.route("/categories", methods=['GET'])
+  def retrieve_categories():
+    categories = Category.query.order_by(Category.type).all()
+
+    if len(categories) == 0:
+      abort(404)
+
+    return jsonify ({
+      "success": True,
+      "categories": {category.id : category.type for category in categories},
+    })
+
+
+
+  #Create an endpoint to handle GET requests for questions, 
   @app.route("/questions")
   def retrieve_questions():
     selection = Question.query.order_by(Question.id).all()
     current_questions = paginate_questions(request, selection)
+
+    categories = Category.query.order_by(Category.type).all()
 
     if len(current_questions) == 0:
       abort(404)
@@ -56,6 +76,8 @@ def create_app(test_config=None):
     })
 
 
+
+  #Create an endpoint to DELETE question using a question ID. 
   @app.route("/questions/<int:question_id>", methods=["DELETE"])
   def delete_question(question_id):
     try:
@@ -79,6 +101,9 @@ def create_app(test_config=None):
       abort(422)
 
 
+
+  #Create an endpoint to POST a new question
+  #Create a POST endpoint to get questions based on a search term. 
   @app.route("/questions", methods=["POST"])
   def create_question():
     body = request.get_json()
@@ -124,8 +149,13 @@ def create_app(test_config=None):
       abort(422)
 
 
+
+  #Create a GET endpoint to get questions based on category.
   @app.route("/categories/<int:categories_id>/questions", methods=['GET'])
-  def get_categories(category_id):
+  def get_question_by_category(category_id):
+    category = Category.query.get(id)
+    if (category is None):
+      abort(404)
 
     try:
       question = Question.query.filter(Question.category == str(category_id)).all()
@@ -143,89 +173,73 @@ def create_app(test_config=None):
 
 
 
+  #Create a POST endpoint to get questions to play the quiz. 
+  @app.route("/questions", methods=["POST"])
+  def quiz_question():
+    body = request.get_json()
+
+    previous_questions = body.get('question', None)
+    quiz_category = body.get('quiz_category', None)
+
+    if quiz_category:
+        if quiz_category['id'] == 0:
+          quiz = Question.query.all()
+        else:
+          quiz = Question.query.filter_by(category = quiz_category['id']).all()
+    if not quiz:
+        abort(422)
 
 
+    selected = []
+    for question in quiz:
+      if question.id not in previous_questions:
+        selected.append(question.format())
+
+    if len(selected) != 0:
+      result = random.choice(selected)
+      return jsonify({
+        'question': result
+      })
+
+    else:
+      return jsonify({
+        'question': False
+      })
+      
 
 
+  # Create error handlers for all expected errors including 404 and 422. 
+  @app.errorhandler(404)
+  def not_found(error):
+    return (
+      jsonify({"success": False, "error": 404, "message": "resource not found"}),
+      404,
+    )
+      
 
-  '''
-  @TODO: DONE!!!
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return (
+      jsonify({"success": False, "error": 422, "message": "unprocessable"}),
+      422,
+    )
 
+        
+  @app.errorhandler(400)
+  def bad_request(error):
+    return (
+      jsonify({"success": False, "error": 400, "message": "bad request"}), 
+      400,
+    )
 
-  '''
-  @TODO: ?????
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories.
-
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
-
-  '''
-  @TODO: DONE!!!!
-  Create an endpoint to DELETE question using a question ID. 
-
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
-
-  '''
-  @TODO:  DONE!!!!
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
-
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
-
-  '''
-  @TODO: DONE!!!!
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
-
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
-
-  '''
-  @TODO: PARTIALLY - DONE
-  Create a GET endpoint to get questions based on category. 
-
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
+  @app.errorhandler(405)
+  def not_allowed(error):
+    return (
+      jsonify({"success": False, "error": 405, "message": "Method Not Allowed"}),
+      405,
+    )
 
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
-
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
-  
   return app
 
     
